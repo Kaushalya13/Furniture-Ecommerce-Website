bd8e8ef150454a9129435065dece650ea4ed0820
Kaushalya13 <nimanthikaushalya13@gmail.com>
2025-10-28 12:34:58 +0530

    change details

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,31 @@
-import Image from "next/image";
+import Navbar from "@/sections/Navbar";
+
+import Hero from "@/sections/Hero";
+import Shop from "@/sections/Shop";
+import Categories from "@/sections/Categories";
+import About from "@/sections/AboutUs";
+import { Suspense } from 'react';
+import Contact from "@/sections/Contact";
+import Footer from "@/sections/Footer";
+import Reviews from "@/sections/Reviews";
+
+
+// A simple loading component
+function LoadingFallback() {
+  return <div className="pt-24 h-screen w-full text-center">Loading products...</div>;
+}
 
 export default function Home() {
   return (
-    <h1>Home page</h1>
+    <>
+    <Navbar />
+    <Hero />
+    <Shop />
+    <Categories />
+    <About />
+    <Reviews />
+    <Contact />
+    <Footer />
+    </>
   );
 }
